41c6de1b498f80a081b6a6d809c4f67a8a0bb682
Leonardo Ramírez Gutiérrez <leoragut@gmail.com>
2023-11-18 01:37:39 -0600

    MiniBoss2 - pt.1

--- Assets/Scripts/Enemigos/MiniBoss.cs
+++ Assets/Scripts/Enemigos/MiniBoss.cs
@@ -6,19 +6,46 @@ public class MiniBoss : MonoBehaviour
 {
     public PlayerCombat playerScript;
     public Transform playerPos;
-    private int damage = 2;
+   
     public int health; 
     private int maxHealth = 10;
+
     public bool isFlipped = false;
+
+    public int attackDamage = 2;
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
 
     private void Start()
     {
         health = maxHealth;
     }
+    void OnDrawGizmosSelected()
+    {
+        // Calcula la posición del punto de ataque
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
 
+        // Dibuja un gizmo esférico para representar el área de ataque
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
     public void MiniBossAttack()
     {
-        playerScript.TakeDamage(damage);
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.x;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+
+        if (colInfo != null)
+        {
+            playerScript.TakeDamage(attackDamage);
+            Debug.Log("Boss Hit");
+        }
+        
     }
     public void LookAtPlayer()
     {
